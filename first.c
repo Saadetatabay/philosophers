188a2cd7a3fd873cfcf6385e0de0cd954e77f611
Saadetatabay <etatabaysaad143@gmail.com>
2025-11-08 23:26:48 +0300

    mutex thread calisma

--- first.c
+++ first.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
+#include <pthread.h>
 #include <unistd.h>
+
+int num = 0;
+pthread_mutex_t ledger_lock;
+void *ledfer(void *data)
+{
+    pthread_mutex_lock(&ledger_lock);
+    num+=1;
+    pthread_mutex_unlock(&ledger_lock);
+    return NULL;
+}
+
 int main()
 {
-    printf("helloo\n");
-    sleep(5);
+    int i;
+    pthread_mutex_init(&ledger_lock,NULL);
+    pthread_t persons[5000];
+    
+    for(i=0;i<5000;i++)
+    {
+        pthread_create(persons+i,NULL,ledfer,NULL);
+        
+    }
+    for(i=0;i<5000;i++)
+    {
+        pthread_join(persons[i],NULL);
+    }   
+    printf("%d\n",num);
 }
