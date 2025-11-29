ffcb1a775403a2a0b8b2f25f7666fa503f3b3ffd
Saadetatabay <etatabaysaad143@gmail.com>
2025-11-29 20:05:56 +0300

    struct oluşturuldu

--- philo.h
+++ philo.h
@@ -2,5 +2,30 @@
 # define PHILO_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/time.h>
+
+typedef struct s_data
+{
+	int				num_philo;
+	int				time_die;
+	int				time_eat;
+	int				time_sleep;
+	long			start_time;
+	pthread_mutex_t *forks;
+}t_data;
+
+typedef struct s_philo
+{
+	int id;
+	int	eat_count;
+	long	last_time_eat;
+	pthread_t thread;
+	pthread_mutex_t *left_fork;
+	pthread_mutex_t *right_fork;
+	t_data	data;
+} t_philo;
 
 #endif
