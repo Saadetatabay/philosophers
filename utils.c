50463245c1121736c95145f8c1e3c388272ce4e8
Saadetatabay <etatabaysaad143@gmail.com>
2025-11-29 20:57:00 +0300

    argc deleted

--- utils.c
+++ utils.c
@@ -1,13 +1,13 @@
 #include "philo.h"
 
-void    init_tdata(t_data data, char **arg, int argc)
+void    init_tdata(t_data data, char **arg)
 {
     data.num_philo = ft_atoi(arg[1]);
     data.time_die = ft_atoi(arg[2]);
     data.time_eat = ft_atoi(arg[3]);
     data.time_sleep = ft_atoi(arg[4]);
-    if (argc == 6)
-        data.must_eat = ft_atoi(arg[4]);
+    if (arg[5])
+        data.must_eat = ft_atoi(arg[5]);
     else
-        data.ust_eat = -1;
+        data.must_eat = -1;
 }
